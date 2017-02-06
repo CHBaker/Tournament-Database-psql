b8c252f124b81afcd830954b9c4c6cddd0930fca
CHBaker <charlesprobaker@mail.com>
2017-02-06 13:12:21 -0700

    REMOVE print statements for debug

--- tournament_test.py
+++ tournament_test.py
@@ -140,8 +140,6 @@ def testPairings():
                           frozenset([id4, id8]), frozenset([id6, id8])
                           ])
     actual_pairs = set([frozenset([pid1, pid2]), frozenset([pid3, pid4]), frozenset([pid5, pid6]), frozenset([pid7, pid8])])
-    print "\n \n \n \n POSS PAIRS", possible_pairs
-    print "\n \n \n \n ACT PAIRS", actual_pairs
     for pair in actual_pairs:
         if pair not in possible_pairs:
             print 'NOT POSSIBLE PAIRS', pair
